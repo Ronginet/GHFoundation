7f16b2d2183fcf4b1110286e4e22e74ebbf81c5f
Rong <ronginet@RongdeMacBook-Pro.local>
2017-10-06 21:18:38 +0800

    Add Category With UIView+Frame file

---
+++ GHFoundation/GHFoundation/Category/UIView/UIView+Frame.h
@@ -0,0 +1,22 @@
+//
+//  UIView+Frame.h
+//  GHFoundation
+//
+//  Created by Rong on 2017/10/6.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+@end
