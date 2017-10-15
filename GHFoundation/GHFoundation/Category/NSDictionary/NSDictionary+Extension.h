98e0bbdd599acb6e60e449f22f0565f7a8b21fc6
Rong <ronginet@RongdeMacBook-Pro.local>
2017-10-15 23:58:29 +0800

    添加 NSDictionary 的 Extension 类别

---
+++ GHFoundation/GHFoundation/Category/NSDictionary/NSDictionary+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Extension.h
+//  GHFoundation
+//
+//  Created by Rong on 2017/10/15.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Extension)
+
+@end
