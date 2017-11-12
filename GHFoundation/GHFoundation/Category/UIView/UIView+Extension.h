0939449a58529e5616193708b9057ee21f8e781e
Rong <ronginet@RongdeMacBook-Pro.local>
2017-11-12 23:23:53 +0800

    添加 UIview 分类文件

---
+++ GHFoundation/GHFoundation/Category/UIView/UIView+Extension.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Extension.h
+//  GHFoundation
+//
+//  Created by Rong on 2017/11/12.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+
+@end
---
+++ GHFoundation/GHFoundation/Category/UIView/UIView+Extension.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Extension.m
+//  GHFoundation
+//
+//  Created by Rong on 2017/11/12.
+//  Copyright © 2017年 Rong. All rights reserved.
+//
+
+#import "UIView+Extension.h"
+
+@implementation UIView (Extension)
+
+@end
