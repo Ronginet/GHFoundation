fb8f391627c18bfaaad5a564da97e04c5231fe14
Rong <ronginet@rongdemacbook-pro.local>
2018-02-02 00:23:34 +0800

    添加 GHFoundation 主头文件 GHFoundation.h

---
+++ GHFoundation/GHFoundation/GHFoundation.h
@@ -0,0 +1,25 @@
+//
+//  GHFoundation.h
+//  GHFoundation
+//
+//  Created by Rong on 2018/1/31.
+//  Copyright © 2018年 Rong. All rights reserved.
+//
+
+#ifndef GHFoundation_h
+#define GHFoundation_h
+
+// Category
+#import "NSString+Extension.h"
+#import "NSDictionary+Extension.h"
+#import "NSData+Image.h"
+#import "NSDate+Extension.h"
+
+#import "UIView+Frame.h"
+#import "UIView+Extension.h"
+#import "UIColor+Extension.h"
+#import "UIDevice+Extension.h"
+#import "UIImage+Extension.h"
+
+
+#endif /* GHFoundation_h */
